8ad61234ec4bc32b6cdf3ffa1ae3d0ae1ac25a9f
kang <baez0670@naver.com>
2019-11-12 04:37:08 -0800

    10

---
+++ week10/car.h
@@ -0,0 +1,19 @@
+#include "vehicle.h"
+
+class car:public vehicle
+{
+protected:
+	char* car_name;
+
+public:
+	car();
+	car(int, int);
+	car(int, int, char*);
+	car(int, int, char*, bool);
+	char* get_car_name();
+	const char* get_class_name();
+	void set_car_name(char*);
+
+};
+
+
